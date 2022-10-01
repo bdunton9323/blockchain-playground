d59c98bac42f96c7688b88376dfe48c8ea1fe107
Benjamin Dunton <benjamin.dunton@gmail.com>
2022-10-01 08:48:11 -0400

    Added a sample API that returns JSON

--- hello.go
+++ hello.go
@@ -5,8 +5,15 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello World!")
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(200, "Hello World!")
+	})
+
+	api := router.Group("/api")
+	api.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
 
 	router.Run(":3000")
